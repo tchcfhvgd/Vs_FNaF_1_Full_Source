c312cb2e656eb0d6f3c2a73b49d454b9f630565f
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-27 10:58:31 +0800

    Update WeekEditorState.hx

--- source/editors/WeekEditorState.hx
+++ source/editors/WeekEditorState.hx
@@ -339,7 +339,7 @@ class WeekEditorState extends MusicBeatState
 		var isMissing:Bool = true;
 		if(assetName != null && assetName.length > 0) {
 			if( #if MODS_ALLOWED FileSystem.exists(Paths.modsImages('storymenu/' + assetName)) || #end
-			Assets.exists(Paths.image('storymenu/' + assetName), IMAGE)) {
+			Assets.exists(Paths.getPath('images/storymenu/' + assetName + '.png', IMAGE), IMAGE)) {
 				weekThing.loadGraphic(Paths.image('storymenu/' + assetName));
 				isMissing = false;
 			}
