4de0cacab22a71c2c767df4dd23251fe0fcf2890
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-27 13:18:24 +0800

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1543,7 +1543,9 @@ class FunkinLua {
 			luaTrace('musicFadeOut is deprecated! Use soundFadeOut instead.', false, true);
 		});
 
+		#if desktop
 		Discord.DiscordClient.addLuaCallbacks(lua);
+		#end
 
 		call('onCreate', []);
 		#end
