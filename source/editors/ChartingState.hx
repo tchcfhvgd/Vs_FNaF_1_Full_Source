b89a23f20667237db72e0829076c7ad217664499
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-27 10:52:38 +0800

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -215,9 +215,6 @@ class ChartingState extends MusicBeatState
 			addSection();
 			PlayState.SONG = _song;
 		}
-		#if MODS_ALLOWED
-		Paths.destroyLoadedImages();
-		#end
 
 		#if desktop
 		// Updating Discord Rich Presence
