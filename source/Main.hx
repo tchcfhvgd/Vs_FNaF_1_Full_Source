a76374a898728d4abe00800c4666eaf6968b7a45
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-27 11:02:32 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -25,6 +25,8 @@ class Main extends Sprite
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var fpsVar:FPS;
 
+	public static var skipNextDump:Bool = false;
+
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
@@ -75,6 +77,18 @@ class Main extends Sprite
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
+		
+		FlxG.signals.preStateSwitch.add(function () {
+			if (!Main.skipNextDump) {
+				Paths.clearStoredMemory(true);
+				FlxG.bitmap.dumpCache();
+			}
+		});
+		FlxG.signals.postStateSwitch.add(function () {
+			Paths.clearUnusedMemory();
+			Main.skipNextDump = false;
+		});
+		
 		addChild(fpsVar);
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
