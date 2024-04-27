50d99c912fe227307050313c1801fec1c8c8414c
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-04-27 10:55:19 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -52,10 +52,6 @@ class FreeplayState extends MusicBeatState
 
 	override function create()
 	{
-		#if MODS_ALLOWED
-		Paths.destroyLoadedImages();
-		#end
-		
 		PlayState.isStoryMode = false;
 		WeekData.reloadWeekFiles(false);
 
