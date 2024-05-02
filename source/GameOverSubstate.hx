ec59ead2160eb488ea71b0699f178867c4a437d9
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-02 10:49:38 +0800

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -98,6 +98,7 @@ class GameOverSubstate extends MusicBeatSubstate
 			FlxG.sound.music.stop();
 			PlayState.deathCounter = 0;
 			PlayState.seenCutscene = false;
+			PlayState.qqqeb = false;
 
 			if (PlayState.isStoryMode)
 				MusicBeatState.switchState(new StoryMenuState());
