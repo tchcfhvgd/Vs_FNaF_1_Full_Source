2f6f718b5e48afe819a2a2d7879a6fabf8a34e2d
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-19 05:55:08 +0800

    Update SUtil.hx

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -1,6 +1,17 @@
 package mobile.backend;
 
 import lime.system.System as LimeSystem;
+if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
+using StringTools;
 
 /**
  * A storage class for mobile.
