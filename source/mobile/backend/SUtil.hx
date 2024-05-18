c2ed344f5574d888db4c05f46c9b6f65b226a0b6
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-19 06:03:58 +0800

    Update SUtil.hx

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -1,7 +1,7 @@
 package mobile.backend;
 
 import lime.system.System as LimeSystem;
-if android
+#if android
 import android.content.Context as AndroidContext;
 import android.widget.Toast as AndroidToast;
 import android.os.Environment as AndroidEnvironment;
@@ -9,6 +9,13 @@ import android.Permissions as AndroidPermissions;
 import android.Settings as AndroidSettings;
 import android.Tools as AndroidTools;
 import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
+#if sys
+import sys.*;
+import sys.io.*;
+#elseif js
+import js.html.*;
 #end
 
 using StringTools;
