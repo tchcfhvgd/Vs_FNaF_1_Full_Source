39975d82e0a36a057b012e454dd82bd651a42d29
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-19 15:58:18 +0800

    Update modchart.lua

--- example_mods/data/broken-inside/modchart.lua
+++ example_mods/data/broken-inside/modchart.lua
@@ -1,13 +1,3 @@
-local allowCountdown = false
-function onStartCountdown()
-	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('cutscene3f');
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
-
 function opponentNoteHit()
     health = getProperty('health')
     if getProperty('health') > 0.1 then
