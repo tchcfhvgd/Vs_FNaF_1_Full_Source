baaf81ab7631ee572a925e3143705c6a80732cd9
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-19 15:59:04 +0800

    Delete example_mods/data/lost-at-sea/script.lua

--- example_mods/data/lost-at-sea/script.lua
+++
@@ -1,9 +0,0 @@
-local allowCountdown = false
-function onStartCountdown()
-	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('cutscene3p');
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
