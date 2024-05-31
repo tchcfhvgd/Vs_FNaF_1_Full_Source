5cd4b6ceb970ce8c8a04f6b028b485297998c0de
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-31 18:46:50 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -33,7 +33,7 @@ class Main extends Sprite
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	public static var fpsVar:FPS;
+	public static var fpsVar:FPSCounter;
 
 	public static var skipNextDump:Bool = false;
 
